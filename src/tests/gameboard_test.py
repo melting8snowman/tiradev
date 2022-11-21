cab9be0579e36b6303907c2b48e276c4fb49b69e
Harri Nieminen <nieminen.harri@gmail.com>
2022-11-21 14:28:47 +0200

    trying to get unittests to show up

--- src/tests/gameboard_test.py
+++ src/tests/gameboard_test.py
@@ -5,5 +5,13 @@ class Testgameboard(unittest.TestCase):
     def setUp(self):
         self.gameboard = gameboard()
 
-    def test_luotu_gameboard_on_olemassa(self):
+    def test_created_gameboard_exists(self):
         self.assertNotEqual(self.gameboard, None)
+    
+    def get_item_returns_piece(self):
+        self.drop_piece(1, 1, 1)
+        self.assertEqual(self.get_item(1,1), "1")
+    
+    def drop_piece_puts_piece(self):
+        self.drop_piece(1, 1, 1)
+        self.assertEqual(self.gameboard[1][1], 1)
